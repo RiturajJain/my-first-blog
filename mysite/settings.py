9342735843f38ec2b47c81e2b1d6fe8b45424f82
rituraj <rituraj.jain17@gmail.com>
2019-08-26 23:15:51 +0530

    Modified templates to display posts form database

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h9vy5$8w!=nfc=%&gj9j4rrubb%gsj*#)r_*(jan%=+eg*#p!g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
